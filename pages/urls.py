52226bc76c36b0ad8c17a34e1bd24a77987e6141
Steve <58792824+wintermutenn@users.noreply.github.com>
2025-12-15 23:11:04 +0300

    Delete pages directory

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('about/', views.about, name='about'),   # Страница "О проекте"
-    path('rules/', views.rules, name='rules'),   # Страница "Правила"
-]
--- pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def about(request):
-    """Страница 'О проекте'."""
-    return render(request, 'about.html')
-
-def rules(request):
-    """Страница 'Правила'."""
-    return render(request, 'rules.html')
